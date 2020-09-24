1cd82f380e05fd52bcfec051ad0a3658aad5ac07
James Reed <jamesreed@fb.com>
2020-09-23 17:46:06 -0700

    [FX][WIP] Make return a node
    ghstack-source-id: c7eea45023fab1f3fcb3079958dddc3af309c583
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/45245

--- torch/fx/graph.py
+++ torch/fx/graph.py
@@ -67,7 +67,7 @@ def map_arg(a: Argument, fn: Callable[[Node], Argument]) -> Argument:
 
 class Graph:
     def __init__(self):
-        self._nodes : List[Node] = []
+        self._nodes : List[Node] = [Node(self, 'return', 'return', 'return', (), {})]
         self._used_names : Dict[str, int] = {}  # base name -> number
 
     @property
@@ -80,7 +80,10 @@ class Graph:
         """
         val_map : Dict[Node, Node] = {}
         for node in g._nodes:
+            if node.op == 'return':
+                continue
             val_map[node] = self.node_copy(node, lambda n : val_map[n])
+
 
     def _mark_uses(self, a: Argument):
         def add_use(n: Node):
@@ -98,7 +101,8 @@ class Graph:
         self._mark_uses(args)
         self._mark_uses(kwargs)
         n = Node(self, name if name is not None else self._name(target), op, target, args, kwargs)
-        self._nodes.append(n)
+        assert self._nodes[-1].op == 'return'
+        self._nodes.insert(-1, n)
         return n
 
     # sugar for above when you know the op
@@ -148,8 +152,14 @@ class Graph:
         return self.create_node(node.op, node.target, args, kwargs, name)
 
     def output(self, result: Argument):
-        self.result = result
-        self._mark_uses(result)
+        assert len(self._nodes) != 0
+        assert self._nodes[-1].op == 'return'
+        self._nodes[-1].args = result
+
+    @property
+    def result(self):
+        assert self._nodes[-1].op == 'return'
+        return self.nodes[-1].args
 
     def _name(self, target: Target) -> str:
         if callable(target):
@@ -219,10 +229,14 @@ class Graph:
                 assert isinstance(node.target, str)
                 body.append(f'{node.name} = {_format_target(root_module, node.target)}\n')
                 continue
+            elif node.op == 'return':
+                continue
             raise NotImplementedError(f'node: {node.op} {node.target}')
 
         src = ''.join(body)
-        return src, str(self.result), free_vars
+        return_node = self._nodes[-1]
+        assert return_node.op == 'return'
+        return src, str(self._nodes[-1].args), free_vars
 
     def __str__(self) -> str:
         placeholder_names : List[str] = []
@@ -251,6 +265,8 @@ class Graph:
                 return None
             elif n.op == 'get_attr':
                 return f'%{n.name} : [uses={n.uses}] = self.{n.target}'
+            elif n.op == 'return':
+                return f'return {n.args}'
             else:
                 return f'%{n.name} : [uses={n.uses}] = {n.op}[target={n.target}](' \
                        f'args = {format_arg(n.args)}, kwargs = {format_arg(n.kwargs)})'
@@ -262,8 +278,6 @@ class Graph:
         for node_str in node_strs:
             if node_str:
                 s += '\n    ' + node_str
-        if hasattr(self, 'result'):
-            s += f'\n    return {format_arg(self.result)}'
         return s
 
     def lint(self, root : Optional[torch.nn.Module] = None):
@@ -273,6 +287,7 @@ class Graph:
             - Checks Nodes have correct ownership (owned by this graph)
             - Checks Nodes appear in topological order
             - If `root` is provided, checks that `target`s exist in `root`
+            - Check for exactly one `return` node at the end of the Graph
         """
 
         # Check topo order
@@ -287,19 +302,31 @@ class Graph:
                                    f'defined! Please check that Nodes in the graph are topologically ordered\n{self}')
 
         seen_values : Set[Node] = set()
-        for node in self.nodes:
+        return_node_idx : Optional[int] = None
+        for i, node in enumerate(self._nodes):
             if node.graph is not self:
                 raise RuntimeError(f'Node \'{node}\' does not belong to this Graph!')
             map_arg(node.args, lambda arg: check_arg(arg, node))
             map_arg(node.kwargs, lambda arg: check_arg(arg, node))
             seen_values.add(node)
 
+            if node.op == 'return':
+                if return_node_idx:
+                    raise RuntimeError('Multiple return nodes in this graph!')
+                return_node_idx = i
+
+        if return_node_idx is None:
+            raise RuntimeError('This graph had no return node!')
+        elif return_node_idx != len(self._nodes) - 1:
+            raise RuntimeError('Return node was not the last node in the graph!')
+
+
         if hasattr(self, 'result'):
             map_arg(self.result, check_arg)
 
         # Check targets are legit
         if root:
-            for node in self.nodes:
+            for node in self._nodes:
                 if node.op in ['get_attr', 'call_module']:
                     target_atoms = node.target.split('.')
                     m_itr = root
